06b6889f7d5e2265de413ab9cb30148ebcf89982
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-04-17 11:54:24 +0200

    Get rid of subfolder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">"Welcome to the SciLifeLab Data Portal"</h1>
   </div>
 </template>
 
