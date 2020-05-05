d7191210aed47b3c9556e7b20e9fd16d6eb08c12
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2020-05-05 08:00:21 +0200

    Front page mockup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,69 @@
 <template>
-  <div class="home section">
-    <h1 class="title is-1">SciLifeLab Covid Data Portal</h1>
+<div class="home section">
+  <h1 class="title is-1">The Swedish COVID-19 Data Hub</h1>
 
+  <section class="section">
+    <div class="content">
+      <p>
+        This is the Swedish hub of data related to SARS-CoV-2 and COVID-19, including information about how to share, manage and access data.
+      </p>
+    </div>
+  </section>
+  <section class="section">
     <div class="tile is-ancestor">
       <div class="tile is-vertical">
-        <div class="tile is-parent">
-          <div class="tile is-child notification home-tiles is-primary">
-            <p class="title">About the portal</p>
-          </div>
-        </div>
         <div class="tile">
           <div class="tile is-parent">
             <div class="tile is-child notification home-tiles is-info">
-              <p class="title">Data management</p>
+              <p class="title">
+                Access Data
+              </p>
+              <ul>
+                <li>Datasets from Sweden</li>
+                <li>Other data</li>
+              </ul>
             </div>
           </div>
           <div class="tile is-parent">
-            <div class="tile is-child home-tiles notification is-warning">
-              <p class="title">Data</p>
+            <div class="tile is-child home-tiles notification is-link">
+              <p class="title">
+                Share Data
+              </p>
+              <ul>
+                <li>Deposit to repositories</li>
+                <li>Data hosting</li>
+                <li>Support</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </section>
 
-    Contact: <a href="mailto:datacentre@scilifelab.se">datacentre@scilifelab.se</a>
+  <section class="section">
+  <div class="level">
+    <div class="level-item">
+      <div>
+        <p class="heading">Number of datasets:</p>
+        <p class="title">xyz</p>
+      </div>
+    </div>
+    <div class="level-item">
+      <div>
+        <p class="heading">Other statistic:</p>
+        <p class="title">xyz</p>
+      </div>
+    </div>
+    <div class="level-item">
+      <div>
+        <p class="heading">Another statistic:</p>
+        <p class="title">xyz</p>
+      </div>
+    </div>
   </div>
+  </section>
+</div>
 </template>
 
 <script>
